4332a97912743ff395303c41779976fd502a869d
Vincent Lecourt <lecourt.vincent@free.fr>
2017-03-13 10:05:57 +0100

    configuration des routes pour gérer les droits

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ImiecarBundle\ImiecarBundle(),
-            new OCUserBundle\OCUserBundle(),
-//            new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
