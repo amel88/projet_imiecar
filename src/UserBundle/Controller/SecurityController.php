ef65db9a0c459bfe181e5a40f9b6984383fa34b5
Vincent Lecourt <lecourt.vincent@free.fr>
2017-03-13 10:06:47 +0100

    Creation des bundle UserBundle

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -1,18 +1,21 @@
 <?php
-// src/OCUserBundle/Controller/SecurityController.php;
 
-namespace OCUserBundle\Controller;
+namespace UserBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
+    /**
+     * @Route("/login", name="login")
+     */
     public function loginAction(Request $request)
     {
         // Si le visiteur est déjà identifié, on le redirige vers l'accueil
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->redirectToRoute('imiecar');
+            return $this->redirectToRoute('welcome');
         }
 
         // Le service authentication_utils permet de récupérer le nom d'utilisateur
@@ -20,9 +23,17 @@ class SecurityController extends Controller
         // (mauvais mot de passe par exemple)
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        return $this->render('OCUserBundle:Security:login.html.twig', array(
+        return $this->render('UserBundle:Security:login.html.twig', array(
             'last_username' => $authenticationUtils->getLastUsername(),
             'error'         => $authenticationUtils->getLastAuthenticationError(),
         ));
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        return $this->render('ImiecarBundle::index.html.twig');
     }
 }
