9f1e0009450d4d8eec47fff29ff935620046dcbf
Vincent Lecourt <lecourt.vincent@free.fr>
2017-03-19 12:05:05 +0100

    Ajout de l'accesseur ID dans user

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -39,6 +39,17 @@ class User extends BaseUser
         // your own logic
     }
 
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
     /**
      * Set lastname
      *
