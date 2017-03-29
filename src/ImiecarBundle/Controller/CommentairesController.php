85c90a93deb87024463e6534cb1cf465a04f3f90
Unknown <christopher.jacquot@gmail.com>
2017-03-29 16:32:37 +0200

    modif 3

--- src/ImiecarBundle/Controller/CommentairesController.php
+++ src/ImiecarBundle/Controller/CommentairesController.php
@@ -45,7 +45,7 @@ class CommentairesController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $trajet->setIdUtilisateur($this->get('security.token_storage')->getToken()->getUser());
+            $this->getUser();
             $em = $this->getDoctrine()->getManager();
             $em->persist($commentaires);
             $em->flush($commentaires);
