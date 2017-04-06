7b20161241b4f56647bda76c84d4d4d726a975dc
Vincent Lecourt <lecourt.vincent@free.fr>
2017-04-06 23:43:26 +0200

    Gros commit suite a un probleme technique
    travail sur les en general

--- web/connect/js/index.js
+++ web/connect/js/index.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
   $(document).mousemove(function(e){
     mouseX = e.pageX;
     mouseY = e.pageY;
-    traX = ((4 * mouseX) / 570) + 40;
-    traY = ((4 * mouseY) / 570) + 50;
+    traX = ((20 * mouseX) / 570) + 40;
+    traY = ((20 * mouseY) / 570) + 50;
     console.log(traX);
     $(".title").css({"background-position": traX + "%" + traY + "%"});
   });
